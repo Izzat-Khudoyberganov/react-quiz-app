54bafb6afb154410aa3185597987cbbe81bc93b9
izzat <izzat@gmail.com>
2024-10-19 12:32:27 +0500

    fixed another one shit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Login, SwitchUserRoute } from "./pages";
-import { UserContext } from "./context/usercontext";
+import { UserContext } from "./context/userContext";
 
 function App() {
     const { user } = useContext(UserContext);
